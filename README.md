0c2346b296944f0a0c744ec91e09555b1c2a2dd7
Sergio González <gonzalez.coser24@triana.salesianos.edu>
2024-12-04 09:33:53 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flutter
+
+Repositorio para recopilar todos los proyectos de Flutter
---
+++ yokesetio/android/app/src/main/kotlin/com/example/yokesetio/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.yokesetio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()

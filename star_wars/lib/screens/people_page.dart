956437b363df5f3f2c8edfbcb6be71ccb8729314
Sergio González <gonzalez.coser24@triana.salesianos.edu>
2024-12-12 12:39:52 +0100

    Se ha conseguido mostrar las cards pero falta mostrar más datos

--- star_wars/lib/screens/people_page.dart
+++ star_wars/lib/screens/people_page.dart
@@ -24,19 +24,45 @@ class _PeoplePageState extends State<PeoplePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 20, 20, 20),
       appBar: AppBar(
-        title: const Text("People"),
+        backgroundColor: Colors.black,
+        title: Container(
+          alignment: Alignment.center,
+          child: const Text(
+            "STAR WHAT???",
+            style: TextStyle(color: Colors.amber, fontFamily: "JediFont"),
+          ),
+        ),
       ),
       body: FutureBuilder<PeopleListResponse>(
           future: peopleResponse,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return _buildPeopleList(snapshot.data!);
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Character list",
+                      style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 22,
+                          fontFamily: "JediFont"),
+                      textAlign: TextAlign.left),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 400,
+                    child: _buildPeopleList(snapshot.data!),
+                  ),
+                ],
+              );
             } else if (snapshot.hasError) {
               return Text("${snapshot.error}");
             }
 
-            return const CircularProgressIndicator();
+            return Container(
+              alignment: Alignment.center,
+              child: const CircularProgressIndicator(),
+            );
           }),
     );
   }
@@ -54,8 +80,36 @@ class _PeoplePageState extends State<PeoplePage> {
   Widget _buildPeopleList(PeopleListResponse peopleResponse) {
     return ListView.builder(
         itemCount: peopleResponse.results!.length,
+        scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return Text(peopleResponse.results![index].name!);
+          return Container(
+              margin: const EdgeInsets.only(top: 100, left: 50, right: 50),
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                      colorFilter: const ColorFilter.mode(
+                          Colors.black38, BlendMode.colorBurn),
+                      image: NetworkImage(
+                          "https://starwars-visualguide.com/assets/img/characters/${index + 1}.jpg"),
+                      fit: BoxFit.cover)),
+              child: Card(
+                color: Colors.transparent,
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                child: Center(
+                  child: Text(
+                    peopleResponse.results![index].name!.toLowerCase(),
+                    style: const TextStyle(
+                        color: Colors.amber,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "JediFont"),
+                  ),
+                ),
+              ));
         });
   }
 }
